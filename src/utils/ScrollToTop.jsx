4584ecce1a3b010ceb19a8b6d9282b68af73fcf8
Dave Barnett <barnett@daves-laptop.lan>
2023-03-26 23:39:01 -0500

    make scroll utility honor anhors

--- src/utils/ScrollToTop.jsx
+++ src/utils/ScrollToTop.jsx
@@ -3,9 +3,16 @@ import { useEffect } from "react";
 import { useLocation } from "react-router";
 
 const ScrollToTop = (props) => {
+  const pathUrl = new URL(window.location.href);
   const location = useLocation();
+
   useEffect(() => {
-    window.scrollTo(0, 0);
+    if (!pathUrl.hash) {
+      window.scrollTo(0, 0);
+    } else {
+      const anchor = document.querySelector(pathUrl.hash)
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    }
   }, [location]);
 
   return <>{props.children}</>
